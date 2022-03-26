54969a73e2b392cdf2885b4e86f6b6411c4c01cd
Kvisus <eternal.wanderer.z@gmail.com>
2022-03-26 13:48:44 +0300

    moved to methods

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import { quotes } from './quotes'
 import  './css/App.css'
@@ -17,18 +16,45 @@ export default class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      color:null,
-      quoteNumber:null
+      color:'FFF',
+      quote:'Lets Go!'
     }
   }
 
   handleClick() { 
-    return 
+    this.colorPicker() 
   }
+
+  colorPicker() {
+    const colorVariables = [
+      "#16a085",
+      "#27ae60",
+      "#2c3e50",
+      "#f39c12",
+      "#e74c3c",
+      "#9b59b6",
+      "#FB6964",
+      "#342224",
+      "#472E32",
+      "#BDBB99",
+      "#77B1A9",
+      "#73A857",
+    ];
+    //let qtnum = Math.floor(Math.random()*quotes.length);
+    this.setState({
+      quote: quotes[Math.floor(Math.random() * quotes.length)],
+      color: colorVariables[Math.floor(Math.random() * colorVariables.length)],
+    });
+  }  
+  componentDidMount(){
+    this.colorPicker();
+  }
+  
+
 
   render() {
     const element = <FontAwesomeIcon icon={faFrog} />;
-    const randNumChooser =()=> {return Math.floor(Math.random()*quotes.length)};
+    /* const randNumChooser =()=> {return Math.floor(Math.random()*quotes.length)};
     let randNum = randNumChooser();
     const colorVariables = [
       "#16a085",
@@ -47,36 +73,34 @@ export default class App extends Component {
     const colorChooser = () => {
       return colorVariables[Math.floor(Math.random() * colorVariables.length)];
     }
-    let colors = colorChooser();
+    let colors = colorChooser(); */
     //let a = 'style={{ "background-color": colors }}';
     return (
-      <div className="wrapper" style={{ "background-color": colors }}>
+      <div className="wrapper" style={{ backgroundColor: this.state.color }}>
         {/* {quotes.map(quote=> quote.quote)} */}
         <div className="container">
-          
-          <div className="quote" style={{ color: colors }}>
-            {quotes[randNum].quote}
+          <div className="quote" style={{ color: this.state.color }}>
+            {this.state.quote.quote}
             {element}
           </div>
-          <div className="author" style={{ color: colors }}>
-            -{quotes[randNum].author}
+          <div className="author" style={{ color: this.state.color }}>
+            -{this.state.quote.author}
           </div>
           <div className="miscHolder">
-            <a href="#" style={{ color: colors }}>
-              <i class="fa-solid fa-frog"></i>
+            <a href="#" style={{ color: this.state.color }}>
+              <i className="fa-solid fa-frog"></i>
               Twitter
             </a>
-            <a href="#" style={{ color: colors }}>
+            <a href="#" style={{ color: this.state.color }}>
               Facebook
             </a>
             <button
               className="button"
-              onClick={randNum}
-              style={{ "background-color": colors }}
+              onClick={this.handleClick.bind(this)}
+              style={{ backgroundColor: this.state.color }}
             >
               New Quote
             </button>
-            
           </div>
         </div>
       </div>
